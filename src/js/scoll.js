cc655ad0283b87194aeeae227991763176303313
IrynaMel <iryna.melnyk270692@gmail.com>
2022-01-12 18:02:23 +0200

    add files

---
+++ src/js/scoll.js
@@ -0,0 +1,11 @@
+export {scroll}
+
+function scroll(){
+const { height: cardHeight } = document
+.querySelector('.gallery')
+.firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+top: cardHeight * 15,
+behavior: 'smooth',
+});}
